8298223cf7d44c1902788355b15fc25edde2d5c5
horwellmv <horwellmv@gmail.com>
2020-11-17 20:01:34 -0300

    Examen terminado, fuera de tiempo, se corrigio todo error y desarrollo cada punto que quedò pendiente, como meta personal a pesar del limite asignado para tal.

--- recuperatorio.py
+++ recuperatorio.py
@@ -1,33 +1,37 @@
 import csv
 import os.path
 
-
+# -------------------- FUNCION PARA CREAR O SOBRE ESCRIBIR UN ARCHIVO .CSV --------------
 
 def guardarLegajo(archivo, datos):
     guardar = "s"
     listLegajos = []
 
     while guardar == "s":
-        legajo = {}
-
-        for dato in datos:
-            legajo[dato] = input(f"Ingrese {dato} : ")
-        listLegajos.append(legajo)
-        print("\tQuiere agregar otro legajo?")
-        guardar = input("\ts/n :  ")
+        legajo = {} # diccionario para agregar a lista de legajos
+        try:
+            for dato in datos:
+                legajo[dato] = input(f"Ingrese {dato} : ")
+            validar = int(legajo["legajo"]) # casteo a entero la clave ingresada como legajo.
+            listLegajos.append(legajo)
+            print("\tQuiere agregar otro legajo?")
+            guardar = input("\ts/n :  ")
+        except ValueError: # atrapo except al no poder castear string a entero.
+            print("\tLegajo debe ser un entero. archivo cancelado.\n")
+            return menu()
 
     try:
-        yaExiste = os.path.isfile(archivo)
+        yaExiste = os.path.isfile(archivo)  # METODO PARA BUSCAR ARCHIVO EXISTENTE
         with open(archivo, 'a', newline='') as file:
-            file_guarda = csv.DictWriter(file, fieldnames=datos)
+            file_guarda = csv.DictWriter(file, fieldnames=datos) # ENCABEZDOS DEL ARCHIVO
 
             if yaExiste:
-                print("\tDesea sobreescribirlo?")
+                print("\tDesea sobreescribirlo?") # SI OPRIME S = OMITE EL ENCABEZADO Y AGREGA LOS NUEVOS LEGAJOS SIN BORRAR
                 action = input("\t s/n: ")
                 if action == "s":
                     file_guarda.writerows(listLegajos)
                 else: return
-            else:
+            else:                # CREA ARCHIVO NUEVO CON ENCABEZADOS Y LEGAJOS
                 file_guarda.writeheader()
                 file_guarda.writerows(listLegajos)
                 return
@@ -36,12 +40,11 @@ def guardarLegajo(archivo, datos):
         print("\tOcurrio un error con el archivo.")
         return
 
-
-
-
+# ---------------------- FUNCION PARA CALCULAR LOS VIATICOS POR LEGAJO A BUSCAR --------------------------------
 
 def mostrarLegajos(archivo1,archivo2, numeroLegajo):
-    with open(archivo1) as fUser, open(archivo2) as fRecursos:
+    try:
+        with open(archivo1) as fUser, open(archivo2) as fRecursos:
             usuarioCSv = csv.reader(fUser, delimiter = ",")
             recursosCSv = csv.reader(fRecursos, delimiter = ",")
 
@@ -53,77 +56,65 @@ def mostrarLegajos(archivo1,archivo2, numeroLegajo):
             gastos = next(recursosCSv, None)
 
             monto = 0
-            limite = 5000 - monto
-            print(numeroLegajo)
-            print(legajo)
+            limite = 5000
+            nombre = ""
 
-            while int(legajo[0]) == numeroLegajo:
+            while legajo: # mietras exista linea con legajo
 
-                print(f"{legajo[0]}, pertenece a {legajo[1]} {legajo[2]}")
-                if not gastos or gastos[0] != legajo[0]:
-                    print("\tNo hay regisros para este usuario")
+                if int(legajo[0]) == numeroLegajo: # si legajo del archivo coincide con el de la busqueda
+                    nombre = (f"{legajo[2]} {legajo[1]}") # rescato nombre asignado al legajo para mostrar en pantalla
 
-                while gastos and gastos[0] == legajo[0]:
-                    print(f"\t{legajo[1]} tiene $: {gastos[1]}")
-                    monto = monto + int(gastos[1])
-                    gastos = next(recursosCSv, None)
-
-                if monto <= 5000:
-                    print(f'\tMonto de viaticos usados por {legajo[1]} {legajo[2]} es de {monto} ')
-
-                else:
-                    print(f"\tMonto de viaticos usados por {legajo[1]} {legajo[2]} es de {monto} y supero por {limite}")
-
+                    while gastos: # recorro lista de recursos humanos con gastos de viaticos
+                        if int(gastos[0])==numeroLegajo: # evaluo que coincide con el legajo buscado
+                            monto = monto + int(gastos[1]) # sumo los valores que estan en el index 1 de gastos
+                        gastos = next(recursosCSv, None) # paso de linea en archivo gastos
                 legajo = next(usuarioCSv, None)
 
+            if monto <= 5000:
+                print(f"\tLegajo {numeroLegajo}: {nombre}, gastó {monto} \n \n")
+            else:
+                print(f"\tLegajo {numeroLegajo}: {nombre}, gastó {monto} y se ha pasado del presupuesto por {monto - limite}\n \n")
+            return menu()
+    except FileNotFoundError: # atrapo except en caso de no poder abrir archivo por error en nombre o por que no existe
+        print(f"\tNo se pudo abrir el archivo{archivo1}, o el archivo no existe\n")
+        return menu()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# ---------------------- MENU PRINCIPAL -------------------------------------------
 
 def menu():
     datos = ["legajo", "apellido", "nombre"]
     archivoViaticos = []
 
     while True:
-        print("  Menú: 1. Guardar legajo \n \t2. Mostrar legajos \n \t3. Exit")
+        print("  Menú: 1. Guardar legajos \n \t2. Consultar viaticos por legajo \n \t3. Exit")
         menu = input("\t")
 
 
         if menu == "1":
-            print("\tIngrese nombre para su archivo:")
+            print("\tIngrese nombre para su archivo \n\t(el .csv se agregarà por sistema):")
+            print("\tO ingrese uno existente para agregar legajos \n\t")
             nombreArchivo = input("\t")
             guardarLegajo(f'\t{nombreArchivo}.csv', datos)
 
+
         if menu == "2":
-            print("\tIngrese nombre del archivo a aparear sin .csv:")
+            print("\tIngrese nombre del archivo a aparear sin la extension .csv:")
             nombreArchivo2 = input("\t")
-            print("\tIngrese numero de legajo a revisar: ")
-            numeroRevisar = int(input("\t"))
-            mostrarLegajos(f'\t{nombreArchivo2}.csv',"viaticos.csv",numeroRevisar)
+            print("\tIngrese numero de legajo que quiere consultar: ")
+
+            try:
+                numeroRevisar = int(input("\t")) # evaluo que sea un entero el jegajo a buscar
+                mostrarLegajos(f'\t{nombreArchivo2}.csv',"viaticos.csv",numeroRevisar)
+
+            except ValueError:
+                print("\tLegajo debe ser un entero. busqueda cancelada.\n")
+
 
         if menu == "3":
             exit()
 
-        else: print("\tRegreso a Menu!..\n \n")
-
+        else:
+            print("")
 
 
 menu()
